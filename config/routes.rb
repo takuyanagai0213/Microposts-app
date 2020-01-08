8aa6aaa0934d0a4999e962ad112924892698aef2
takuya.nagai <nagataku021@outlook.jp>
2020-01-08 11:31:08 +0000

    user login

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   root to: 'toppages#index'
+  
+  get 'login', to: 'session#new'
+  post 'login', to: 'session#create'
+  delete 'logout', to: 'session#destroy'
 
   get 'signup', to: 'users#new'
   resources :users, only: [:index, :show, :new, :create]
